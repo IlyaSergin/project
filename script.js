0023010d378c839f970af4ab7cf7196256381a93
IlyaSeregin <ilyaaaseregin@gmail.com>
2024-04-20 17:16:35 +0300

    add base

---
+++ script.js
@@ -0,0 +1,13 @@
+let butts = document.querySelectorAll(".var")
+let mainq = document.querySelector(".ques")
+let stat = document.querySelector(".stat")
+let start = document.querySelector(".start")
+let base = document.querySelector(".base")
+stat.style.display = "none"
+base.style.display = "none"
+for (let i = 0; i < butts.length; i++) {
+    butts[i].addEventListener("click", function () {
+
+    }
+    )
+}
